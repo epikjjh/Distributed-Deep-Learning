8c785d1e558f9417c5bc362d93c97b7b2a333273
2015004248 <epikjjh@gmail.com>
2020-07-26 17:12:43 +0900

    Refactoring: Milestone To do: separate worker/parameter model -> differenct tensor graph separate mpi rank avoid hardcoding
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

--- mnist/model.py
+++ mnist/model.py
@@ -4,13 +4,11 @@ import numpy as np
 
 class Model:
     def __init__(self):
-        # Data: mnist dataset
-        self.data = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
         # CNN model
         self.x = tf.placeholder(tf.float32, [None, 784])
         self.x_image = tf.reshape(self.x, [-1,28,28,1])
         self.y_ = tf.placeholder(tf.float32, [None, 10])
+
         '''First Conv layer'''
         # shape: [5,5,1,32]
         self.w_conv1 = tf.Variable(tf.truncated_normal([5,5,1,32], stddev=0.1)) 
@@ -32,6 +30,7 @@ class Model:
         # activation layer
         self.h_conv2 = tf.nn.relu(self.conv2 + self.b_conv2)
         self.h_pool2 = tf.nn.max_pool(self.h_conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+        
         '''FC layer'''
         self.w_fc1 = tf.Variable(tf.truncated_normal([7*7*64, 1024], stddev=0.1))
         self.b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
@@ -50,14 +49,8 @@ class Model:
         '''Cost function & optimizer'''
         self.cost = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(self.y), reduction_indices=[1]))
         self.optimizer = tf.train.AdamOptimizer(1e-4)
-        self.train_step = self.optimizer.minimize(self.cost)
 
-        # Gradients
-        self.grads = self.optimizer.compute_gradients(self.cost, 
-                        [self.w_conv1, self.b_conv1,
-                        self.w_conv2, self.b_conv2,
-                        self.w_fc1, self.b_fc1,
-                        self.w_fc2, self.b_fc2])
+        '''
         # Data1
         self.d1_conv1_gw = np.empty((5,5,1,32), dtype=np.float32) 
         self.d1_conv1_gb = np.empty(32, dtype=np.float32) 
@@ -85,13 +78,9 @@ class Model:
         self.bcast_fc1_b = np.empty(1024, dtype=np.float32) 
         self.bcast_fc2_w = np.empty((1024,10), dtype=np.float32) 
         self.bcast_fc2_b = np.empty(10, dtype=np.float32) 
+        '''
 
-        # For evaluating
-        self.prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.prediction, tf.float32))
-        self.test_x = self.data.test.images
-        self.test_y_ = self.data.test.labels 
-
+    def draw_graph(self):
         # Create session
         self.sess = tf.Session()
 
@@ -99,6 +88,36 @@ class Model:
         self.sess.run(tf.global_variables_initializer())
 
 
-    def train(self):
-        # Train specific model here
-        raise NotImplementedError("Train method not implemented")
+class Worker(Model):
+    def __init__(self):
+        super().__init__()
+        # For evaluating
+        self.prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.prediction, tf.float32))
+        self.test_x = self.data.test.images
+        self.test_y_ = self.data.test.labels 
+        
+        # Data: mnist dataset
+        self.data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        # Gradients
+        self.grads = self.optimizer.compute_gradients(self.cost, 
+                        [self.w_conv1, self.b_conv1,
+                        self.w_conv2, self.b_conv2,
+                        self.w_fc1, self.b_fc1,
+                        self.w_fc2, self.b_fc2])
+
+
+
+
+class ParameterServer(Model):
+    def __init__(self):
+        super().__init__()
+        self.optimizer = tf.train.AdamOptimizer(1e-4)
+        # Data
+        self.feature_update = self.optimizer.apply_gradients((
+                    (vars[0], self.w_conv1), (vars[1], self.b_conv1),
+                    (vars[2], self.w_conv2), (vars[3], self.b_conv2),
+                    (vars[4], self.w_fc1), (vars[5], self.b_fc1),
+                    (vars[6], self.w_fc2), (vars[7], self.b_fc2),
+                ))
+
