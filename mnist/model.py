ba2f40b69e05a97c449038dec57644fa1450e6fc
2015004248 <epikjjh@gmail.com>
2020-08-02 18:53:42 +0900

    Refactoring To Do: several errors(NaN problem)
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

--- mnist/model.py
+++ mnist/model.py
@@ -4,73 +4,86 @@ import numpy as np
 
 class Model:
     def __init__(self):
-         # Data: mnist dataset
+        # Data: mnist dataset
         self.data = input_data.read_data_sets("MNIST_data/", one_hot=True)
         
         # CNN model
-        self.x = tf.placeholder(tf.float32, [None, 784])
-        self.x_image = tf.reshape(self.x, [-1,28,28,1])
-        self.y_ = tf.placeholder(tf.float32, [None, 10])
+        with tf.compat.v1.variable_scope("mnist", reuse=tf.compat.v1.AUTO_REUSE):
+            self.x = tf.compat.v1.placeholder(tf.float32, [None, 784])
+            self.x_image = tf.reshape(self.x, [-1,28,28,1])
+            self.y_ = tf.compat.v1.placeholder(tf.float32, [None, 10])
 
-        '''First Conv layer'''
-        # shape: [5,5,1,32]
-        self.w_conv1 = tf.get_variable("v0", shape=[5,5,1,32], initializer=tf.random_normal_initializer(), dtype=tf.float32) 
-        # shape: [32]
-        self.b_conv1 = tf.get_variable("v1", shape=[32], initializer=tf.random_normal_initializer(), dtype=tf.float32)
-        # conv layer
-        self.conv1 = tf.nn.conv2d(self.x_image, self.w_conv1, strides=[1,1,1,1], padding='SAME')
-        # activation layer
-        self.h_conv1 = tf.nn.relu(self.conv1 + self.b_conv1)
-        self.h_pool1 = tf.nn.max_pool(self.h_conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+            '''First Conv layer'''
+            # shape: [5,5,1,32]
+            self.w_conv1 = tf.compat.v1.get_variable("v0", shape=[5,5,1,32], dtype=tf.float32) 
+            # shape: [32]
+            self.b_conv1 = tf.compat.v1.get_variable("v1", shape=[32], dtype=tf.float32)
+            # conv layer
+            self.conv1 = tf.nn.conv2d(self.x_image, self.w_conv1, strides=[1,1,1,1], padding='SAME')
+            # activation layer
+            self.h_conv1 = tf.nn.relu(self.conv1 + self.b_conv1)
+            self.h_pool1 = tf.nn.max_pool2d(self.h_conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-        '''Second Conv layer'''
-        # shape: [5,5,32,64] 
-        self.w_conv2 = tf.get_variable("v2", shape=[5,5,32,64], initializer=tf.random_normal_initializer(), dtype=tf.float32)
-        # shape: [64] 
-        self.b_conv2 = tf.get_variable("v3", shape=[64], initializer=tf.random_normal_initializer(), dtype=tf.float32)
-        # conv layer
-        self.conv2 = tf.nn.conv2d(self.h_pool1, self.w_conv2, strides=[1,1,1,1], padding='SAME')
-        # activation layer
-        self.h_conv2 = tf.nn.relu(self.conv2 + self.b_conv2)
-        self.h_pool2 = tf.nn.max_pool(self.h_conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+            '''Second Conv layer'''
+            # shape: [5,5,32,64] 
+            self.w_conv2 = tf.compat.v1.get_variable("v2", shape=[5,5,32,64], dtype=tf.float32)
+            # shape: [64] 
+            self.b_conv2 = tf.compat.v1.get_variable("v3", shape=[64], dtype=tf.float32)
+            # conv layer
+            self.conv2 = tf.nn.conv2d(self.h_pool1, self.w_conv2, strides=[1,1,1,1], padding='SAME')
+            # activation layer
+            self.h_conv2 = tf.nn.relu(self.conv2 + self.b_conv2)
+            self.h_pool2 = tf.nn.max_pool2d(self.h_conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+            
+            '''FC layer'''
+            self.w_fc1 = tf.compat.v1.get_variable("v4", shape=[7*7*64, 1024], dtype=tf.float32)
+            self.b_fc1 = tf.compat.v1.get_variable("v5", shape=[1024], dtype=tf.float32)
+            self.h_pool2_flat = tf.reshape(self.h_pool2, [-1, 7*7*64])
+            self.h_fc1 = tf.nn.relu(tf.matmul(self.h_pool2_flat, self.w_fc1) + self.b_fc1)
+
+            '''Dropout'''
+            self.keep_prob = tf.compat.v1.placeholder(tf.float32)
+            self.h_fc1_drop = tf.nn.dropout(self.h_fc1, rate=1.0-self.keep_prob)
+
+            '''Softmax layer'''
+            self.w_fc2 = tf.compat.v1.get_variable("v6", shape=[1024, 10], dtype=tf.float32)
+            self.b_fc2 = tf.compat.v1.get_variable("v7", shape=[10], dtype=tf.float32)
+            self.logits = tf.matmul(self.h_fc1_drop, self.w_fc2) + self.b_fc2
+            self.y = tf.nn.softmax(self.logits)
+
+            '''Cost function & optimizer'''
+            self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y_)
+            self.cost = tf.reduce_mean(self.loss)
+            self.optimizer = tf.compat.v1.train.AdamOptimizer(1e-4)
         
-        '''FC layer'''
-        self.w_fc1 = tf.get_variable("v4", shape=[7*7*64, 1024], initializer=tf.random_normal_initializer(), dtype=tf.float32)
-        self.b_fc1 = tf.get_variable("v5", shape=[1024], initializer=tf.random_normal_initializer(), dtype=tf.float32)
-        self.h_pool2_flat = tf.reshape(self.h_pool2, [-1, 7*7*64])
-        self.h_fc1 = tf.nn.relu(tf.matmul(self.h_pool2_flat, self.w_fc1) + self.b_fc1)
+            # Variables
+            self.var_size = 8
+            self.var_shape = [
+                [5,5,1,32],
+                [32],
+                [5,5,32,64],
+                [64],
+                [7*7*64, 1024],
+                [1024],
+                [1024, 10],
+                [10]
+            ]
+            self.var_bucket = [tf.compat.v1.get_variable("v{}".format(i), shape=self.var_shape[i], dtype=tf.float32) for i in range(self.var_size)] 
 
-        '''Dropout'''
-        self.keep_prob = tf.placeholder(tf.float32)
-        self.h_fc1_drop = tf.nn.dropout(self.h_fc1, self.keep_prob)
+            # Gradients
+            self.grads = self.optimizer.compute_gradients(self.cost, self.var_bucket)
 
-        '''Softmax layer'''
-        self.w_fc2 = tf.get_variable("v6", shape=[1024, 10], initializer=tf.random_normal_initializer(), dtype=tf.float32)
-        self.b_fc2 = tf.get_variable("v7", shape=[10], initializer=tf.random_normal_initializer(), dtype=tf.float32)
-        self.y = tf.nn.softmax(tf.matmul(self.h_fc1_drop, self.w_fc2) + self.b_fc2)
+            # For evaluating
+            self.prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(self.prediction, tf.float32))
+            self.test_x = self.data.test.images
+            self.test_y_ = self.data.test.labels
+            self.train_step = self.optimizer.minimize(self.cost)
+            
+            # Create session
+            self.sess = tf.compat.v1.Session()
 
-        '''Cost function & optimizer'''
-        self.cost = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(self.y), reduction_indices=[1]))
-        self.optimizer = tf.train.AdamOptimizer(1e-4)
-        
-        # Variables
-        self.var_size = 8
+            # Initialize variables
+            self.sess.run(tf.compat.v1.global_variables_initializer())
 
-        # Gradients
-        self.grads = self.optimizer.compute_gradients(self.cost, 
-                        [self.w_conv1, self.b_conv1,
-                        self.w_conv2, self.b_conv2,
-                        self.w_fc1, self.b_fc1,
-                        self.w_fc2, self.b_fc2])
-
-        # For evaluating
-        self.prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.prediction, tf.float32))
-        self.test_x = self.data.test.images
-        self.test_y_ = self.data.test.labels 
-        
-        # Create session
-        self.sess = tf.Session()
-
-        # Initialize variables
-        self.sess.run(tf.global_variables_initializer())
+            
--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@ from mpi4py import MPI
 from typing import List
 import numpy as np
 import tensorflow as tf
-import time
+import time,sys
 
 class SyncWorker(Model):
     def __init__(self, batch_size):
@@ -11,18 +11,24 @@ class SyncWorker(Model):
         self.batch_size = batch_size
 
     def work(self) -> List:
-        x_batch, y_batch = self.data.train.next_batch(self.batch_size)
-        # Compute gradient values
-        ret, = self.sess.run([self.grads], feed_dict={self.x: x_batch, self.y_: y_batch, self.keep_prob: 0.5})
-        
-        # ret -> ((gw, w), (gb, b))
-        grads = [grad for grad, var in ret]
+        with tf.compat.v1.variable_scope("mnist", reuse=tf.compat.v1.AUTO_REUSE):
+            x_batch, y_batch = self.data.train.next_batch(self.batch_size)
+            # Compute gradient values
+            ret, = self.sess.run([self.grads], feed_dict={self.x: x_batch, self.y_: y_batch, self.keep_prob: 0.5})
+            
+            # ret -> ((gw, w), (gb, b))
+            #print(ret[-1])
+            '''
+            Problem: nan gradients
+            print(ret[-1])
+            '''
+            grads = [grad for grad, var in ret]
 
-        # Tuple: (gradient, variable)
-        # Pack gradeint values 
-        # Data: [gw_conv1, gb_conv1, gw_conv2, gb_conv2, gw_fc1, gb_fc1, gw_fc2, gb_fc2]
-        # Send data to parameter server
-        return grads
+            # Tuple: (gradient, variable)
+            # Pack gradeint values 
+            # Data: [gw_conv1, gb_conv1, gw_conv2, gb_conv2, gw_fc1, gb_fc1, gw_fc2, gb_fc2]
+            # Send data to parameter server
+            return grads
 
 
 class ParameterServer:
@@ -42,56 +48,119 @@ class ParameterServer:
         # Data for worker
         # For worker1
         self.w1_bucket = [np.empty(self.var_shape[i], dtype=np.float32) for i in range(self.var_size)]
+
         # For worker2
         self.w2_bucket = [np.empty(self.var_shape[i], dtype=np.float32) for i in range(self.var_size)]
+
+        # For worker1 + worker2 
+        self.w_bucket = [np.empty(self.var_shape[i], dtype=np.float32) for i in range(self.var_size)]
     
         # TF variables
-        self.var_bucket = [tf.get_variable("v{}".format(i), shape=self.var_shape[i], initializer=tf.random_normal_initializer(), dtype=tf.float32) for i in range(self.var_size)]
+        with tf.compat.v1.variable_scope("ParameterServer", reuse=tf.compat.v1.AUTO_REUSE):
+            self.var_bucket = [tf.compat.v1.get_variable("v{}".format(i), shape=self.var_shape[i], dtype=tf.float32) for i in range(self.var_size)]
 
-        # Optimizer
-        self.optimizer = tf.train.AdamOptimizer(1e-4)
+            # Optimizer
+            self.optimizer = tf.compat.v1.train.AdamOptimizer(1e-4)
 
-        # Apply gradients
-        self.grads_and_vars = [(self.w1_bucket[i]+self.w2_bucket[i], self.var_bucket[i]) for i in range(self.var_size)]
-        self.sync_gradients = self.optimizer.apply_gradients(self.grads_and_vars)
-        
-        # Create session
-        self.sess = tf.Session()
-        self.sess.run(tf.global_variables_initializer())
+            # Apply gradients
+            # Tuple: (gradient, variable)
+            # Pack gradeint values 
+            self.grads_and_vars = [(self.w_bucket[i], self.var_bucket[i]) for i in range(self.var_size)]
+            self.sync_gradients = self.optimizer.apply_gradients(self.grads_and_vars)
+            
+            # Create session
+            self.sess = tf.compat.v1.Session()
+            self.sess.run(tf.compat.v1.global_variables_initializer())
 
     def update(self):
-        self.sess.run(self.sync_gradients)
+        for i in range(self.var_size):
+            self.w_bucket[i] = self.w1_bucket[i] + self.w2_bucket[i]
+
+        with tf.compat.v1.variable_scope("ParameterServer", reuse=tf.compat.v1.AUTO_REUSE):
+            self.sess.run(self.sync_gradients)
 
 
 if __name__ == "__main__":
-    epoch = 1
-    batch_size = 100
+    epoch = 500
+    batch_size = 50
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank() 
     
     # Worker1
     if rank == 1:
+        start = time.clock()
+
         w1 = SyncWorker(batch_size) 
-        for step in range(epoch):
-            grads_w1 = w1.work()
+        # For broadcasting 
+        bucket = [np.empty(w1.var_shape[i], dtype=np.float32) for i in range(w1.var_size)]
+
+        with tf.compat.v1.variable_scope("mnist", reuse=tf.compat.v1.AUTO_REUSE):
+            var_bucket = [tf.compat.v1.get_variable("v{}".format(i), shape=w1.var_shape[i], dtype=tf.float32) for i in range(w1.var_size)]
+            bucket_assign = [tf.compat.v1.assign(var_bucket[i], bucket[i]) for i in range(w1.var_size)]
+
+            for step in range(epoch):
+                grads_w1 = w1.work()
+                '''
+                Problem: nan
+                print(grads_w2)
+                '''
+
+                # Send worker 1's grads
+                for i in range(w1.var_size):
+                    comm.Send([grads_w1[i], MPI.DOUBLE], dest=0, tag=i+1)
+
+                # Receive data from parameter server
+                for i in range(w1.var_size):
+                    comm.Bcast([bucket[i], MPI.DOUBLE], root=0) 
+
+                # Assign broadcasted values
+                w1.sess.run(bucket_assign)
             
-            # Send worker 1's grads
-            for i in range(w1.var_size):
-                comm.Send([grads_w1[i], MPI.DOUBLE], dest=0, tag=i+1)
+            end = time.clock()
+            print("Worker{} Accuracy: {}".format(rank,w1.sess.run(w1.accuracy, feed_dict={w1.x: w1.test_x, w1.y_: w1.test_y_, w1.keep_prob: 1.0})))
+            print("Time: {}".format(end-start))
+     
 
     # Worker2
     elif rank == 2:
+        start = time.clock()
+
         w2 = SyncWorker(batch_size) 
-        for step in range(epoch):
-            grads_w2 = w2.work()
-            
-            # Send worker 1's grads
-            for i in range(w2.var_size):
-                comm.Send([grads_w2[i], MPI.DOUBLE], dest=0, tag=i+1)
+        # For broadcasting 
+        bucket = [np.empty(w2.var_shape[i], dtype=np.float32) for i in range(w2.var_size)]
+
+        with tf.compat.v1.variable_scope("mnist", reuse=tf.compat.v1.AUTO_REUSE):
+            var_bucket = [tf.compat.v1.get_variable("v{}".format(i), shape=w2.var_shape[i], dtype=tf.float32) for i in range(w2.var_size)]
+            bucket_assign = [tf.compat.v1.assign(var_bucket[i], bucket[i]) for i in range(w2.var_size)]
+
+            for step in range(epoch):
+                grads_w2 = w2.work()
+                '''
+                Problem: nan
+                print(grads_w2)
+                '''
+                
+                # Send worker 1's grads
+                for i in range(w2.var_size):
+                    comm.Send([grads_w2[i], MPI.DOUBLE], dest=0, tag=i+1)
+
+                # Receive data from parameter server
+                for i in range(w2.var_size):
+                    comm.Bcast([bucket[i], MPI.DOUBLE], root=0) 
+
+                # Assign broadcasted values
+                w2.sess.run(bucket_assign)
+
+            end = time.clock()
+            print("Worker{} Accuracy: {}".format(rank,w2.sess.run(w2.accuracy, feed_dict={w2.x: w2.test_x, w2.y_: w2.test_y_, w2.keep_prob: 1.0})))
+            print("Time: {}".format(end-start))
 
     # Parameter Server
     else:
         ps = ParameterServer()
+        # For broadcasting 
+        bucket = [np.empty(ps.var_shape[i], dtype=np.float32) for i in range(ps.var_size)]
+
         for step in range(epoch):
             # Receive data from workers
             # From worker1
@@ -102,79 +171,26 @@ if __name__ == "__main__":
             for i in range(ps.var_size):
                 comm.Recv([ps.w2_bucket[i], MPI.DOUBLE], source=2, tag=i+1)
 
+            '''
+            Problem: nan
+            print(ps.w1_bucket)
+            print(ps.w2_bucket)
+            '''
+
             # Synchronize
             ps.update()
 
 
-    '''
-    ps = ParameterServer(comm, rank) 
-    w1 = SyncWorker(comm, rank, batch_size)
-    w2 = SyncWorker(comm, rank, batch_size) 
-
-    conv1_gw = np.empty((5,5,1,32), dtype=np.float32)
-    conv1_gb = np.empty(32, dtype=np.float32)
-    conv2_gw = np.empty((5,5,32,64), dtype=np.float32)
-    conv2_gb = np.empty(64, dtype=np.float32)
-    fc1_gw = np.empty((7*7*64,1024), dtype=np.float32)
-    fc1_gb = np.empty(1024, dtype=np.float32)
-    fc2_gw = np.empty((1024,10), dtype=np.float32)
-    fc2_gb = np.empty(10, dtype=np.float32)
-
-
-    # Mapping variables
-    vars = [
-        conv1_gw, conv1_gb, conv2_gw, conv2_gb,
-        fc1_gw, fc1_gb, fc2_gw, fc2_gb
-    ]
-        
-    # Measure time
-    if rank != 0:
-        start = time.clock()
-
-    for step in range(training_time):
-        # Parameter Server
-        if rank == 0:
-            vars = ps.sync()
-
-        # Worker1 
-        elif rank == 1:
-            grads_w1 = w1.work()
-
-            # Send worker 1's grads
-            for i in range(8):
-                comm.Send([grads_w1[i], MPI.DOUBLE], dest=0, tag=i+1)
-        
-        # Worker2 
-        else:
-            grads_w2 = w2.work()
-
-            # Send worker 2's grads
-            for i in range(8):
-                comm.Send([grads_w2[i], MPI.DOUBLE], dest=0, tag=i+1)
-
-
-        # Receive data from parameter server
-        comm.Bcast([vars[0], MPI.DOUBLE], root=0) 
-        comm.Bcast([vars[1], MPI.DOUBLE], root=0) 
-        comm.Bcast([vars[2], MPI.DOUBLE], root=0) 
-        comm.Bcast([vars[3], MPI.DOUBLE], root=0) 
-        comm.Bcast([vars[4], MPI.DOUBLE], root=0) 
-        comm.Bcast([vars[5], MPI.DOUBLE], root=0) 
-        comm.Bcast([vars[6], MPI.DOUBLE], root=0) 
-        comm.Bcast([vars[7], MPI.DOUBLE], root=0) 
-        
-        # Update variables (worker)
-        if rank == 1:
-            w1.update(vars)
-
-        elif rank == 2:
-            w2.update(vars)
-
-
-    # Evaluate model
-    if self.rank != 0:
-        end = time.clock()
-        print(w1.sess.run(self.accuracy, feed_dict={w1.x: w1.test_x, w1.y_: w1.test_y_, w1.keep_prob: 1.0}))
-        print(w2.sess.run(self.accuracy, feed_dict={w2.x: w2.test_x, w2.y_: w2.test_y_, w2.keep_prob: 1.0}))
-        print(end-start)
-    '''
+            with tf.compat.v1.variable_scope("ParameterServer", reuse=tf.compat.v1.AUTO_REUSE):
+                # Broadcast values
+                for i in range(ps.var_size):
+                    bucket[i] = ps.var_bucket[i].eval(session=ps.sess)
+                    '''
+                    Problem: same values
+                    if i==7:
+                        print(bucket[i])
+                        print("_____________________________________________________")
+                    '''
+                # send to worker 1, 2
+            for i in range(ps.var_size):
+                comm.Bcast([bucket[i], MPI.DOUBLE], root=0) 
