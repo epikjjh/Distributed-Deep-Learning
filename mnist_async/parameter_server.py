d01c125251e55891c51bb5f3defab2b5a460eb3e
2015004248 <epikjjh@gmail.com>
2020-08-26 01:13:28 +0900

    Async mmist model complete Accuracy: about 95% in epoch 1 Time: Reduce 30 seconds compared with Sync mnist model Todo: Abstraction for parameter server / worker arguments
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

---
+++ mnist_async/parameter_server.py
@@ -0,0 +1,68 @@
+from mpi4py import MPI
+import numpy as np
+import tensorflow as tf
+import time,sys
+
+class ParameterServer:
+    def __init__(self, params):
+        self.var_size = params["size"]
+        self.var_shape = params["shape"]
+        self.total_batch = params["total_batch"]
+
+        # Data for worker
+        self.w_bucket = [np.empty(self.var_shape[i], dtype=np.float32) for i in range(self.var_size)]
+        self.ph_bucket = [tf.compat.v1.placeholder(shape=self.var_shape[i], dtype=tf.float32) for i in range(self.var_size)]
+    
+        # TF variables
+        with tf.compat.v1.variable_scope("ParameterServer", reuse=tf.compat.v1.AUTO_REUSE):
+            self.var_bucket = [tf.compat.v1.get_variable("v{}".format(i), shape=self.var_shape[i], dtype=tf.float32) for i in range(self.var_size)]
+
+        # Optimizer
+        self.optimizer = tf.compat.v1.train.AdamOptimizer(1e-4)
+
+        # Apply gradients
+        # Tuple: (gradient, variable)
+        # Pack gradeint values 
+        self.grads_and_vars = [(self.ph_bucket[i], self.var_bucket[i]) for i in range(self.var_size)]
+        self.sync_gradients = self.optimizer.apply_gradients(self.grads_and_vars)
+            
+        # Create session
+        self.sess = tf.compat.v1.Session()
+        self.sess.run(tf.compat.v1.global_variables_initializer())
+
+    def update(self):
+        self.sess.run(self.sync_gradients, feed_dict={self.ph_bucket[i]:self.w_bucket[i] for i in range(self.var_size)})
+
+
+if __name__ == "__main__":
+    epoch = 1
+    batch_size = 100
+    comm = MPI.COMM_WORLD
+    info = MPI.Status()
+    worker_num = comm.Get_size()-1
+
+    # Receive parameters from worker
+    params = comm.recv(source=1, tag=0)
+
+    ps = ParameterServer(params)
+    # For broadcasting 
+    bucket = [np.empty(ps.var_shape[i], dtype=np.float32) for i in range(ps.var_size)]
+
+    for step in range(epoch):
+        # Batch iteration number: batch count * number of worker    
+        batch_num = int(ps.total_batch/batch_size)*worker_num
+        for batch_cnt in range(batch_num):
+            # Receive data from worker
+            for i in range(ps.var_size):
+                comm.Recv([ps.w_bucket[i], MPI.FLOAT], source=MPI.ANY_SOURCE, tag=i+1, status=info)
+                
+            # Synchronize
+            ps.update()
+
+            # Prepare sending values
+            for i in range(ps.var_size):
+                bucket[i] = ps.var_bucket[i].eval(session=ps.sess)
+
+            # send to worker
+            for i in range(ps.var_size):
+                comm.Send([bucket[i], MPI.FLOAT], dest=info.Get_source(), tag=i+1) 
---
+++ mnist_async/worker.py
@@ -0,0 +1,68 @@
+from model import Model
+from mpi4py import MPI
+from typing import List
+import numpy as np
+import tensorflow as tf
+import time,sys
+
+class SyncWorker(Model):
+    def __init__(self, batch_size):
+        super().__init__()
+        self.batch_size = batch_size
+
+    def work(self, cnt) -> List:
+        x_batch = self.x_train[self.batch_size*cnt:self.batch_size*(cnt+1)]
+        y_batch = self.y_train[self.batch_size*cnt:self.batch_size*(cnt+1)]
+
+        # Compute gradient values
+        ret, = self.sess.run([self.grads], feed_dict={self.x: x_batch, self.y_: y_batch, self.keep_prob: 0.5})
+
+        # ret -> ((gw, w), (gb, b))
+        grads = [grad for grad, var in ret]
+        # Tuple: (gradient, variable)
+        # Pack gradeint values 
+        # Data: [gw_conv1, gb_conv1, gw_conv2, gb_conv2, gw_fc1, gb_fc1, gw_fc2, gb_fc2]
+        # Send data to parameter server
+        return grads
+
+
+if __name__ == "__main__":
+    epoch = 1
+    batch_size = 100
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    start = time.clock()
+    worker = SyncWorker(batch_size) 
+
+    # Send parameters to parameter server
+    if rank == 1:
+        data = {"size": worker.var_size, "shape": worker.var_shape, "total_batch": worker.x_train.shape[0]}
+        comm.send(data, dest=0, tag=0)
+
+    # For broadcasting 
+    bucket = [np.empty(worker.var_shape[i], dtype=np.float32) for i in range(worker.var_size)]
+    ph_bucket = [tf.compat.v1.placeholder(shape=worker.var_shape[i], dtype=tf.float32) for i in range(worker.var_size)]
+
+    bucket_assign = [tf.compat.v1.assign(worker.var_bucket[i], ph_bucket[i]) for i in range(worker.var_size)]
+
+    for step in range(epoch):
+        batch_num = int(worker.x_train.shape[0]/batch_size)
+        for batch_cnt in range(batch_num):
+            grads = worker.work(batch_cnt)
+            # Send worker 1's grads
+            for i in range(worker.var_size):
+                comm.Send([grads[i], MPI.FLOAT], dest=0, tag=i+1)
+
+            # Receive data from parameter server
+            for i in range(worker.var_size):
+                comm.Recv([bucket[i], MPI.FLOAT], source=0, tag=i+1) 
+
+            # Assign broadcasted values
+            worker.sess.run(bucket_assign, feed_dict={ph_bucket[i]:bucket[i] for i in range(worker.var_size)})
+            if batch_cnt % 10 == 0:
+                print("Worker{} epoch: {} batch: {} accuracy: {}".format(rank,step,batch_cnt,worker.sess.run(worker.accuracy, feed_dict={worker.x: worker.x_test, worker.y_: worker.y_test, worker.keep_prob: 1.0})))
+    
+    end = time.clock()
+    print("Worker{} final accuracy: {}".format(rank,worker.sess.run(worker.accuracy, feed_dict={worker.x: worker.x_test, worker.y_: worker.y_test, worker.keep_prob: 1.0})))
+    print("Time: {}".format(end-start))
